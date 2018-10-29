f02346718aa068b0c2deedc2788eb508bcc8f71d
Josh Day <emailjoshday@gmail.com>
2018-10-29 15:23:45 -0400

    wip

--- Courses/Data_Science/05-glms.jl
+++ Courses/Data_Science/05-glms.jl
@@ -4,11 +4,14 @@ ENV["GKSwstype"] = "100" #src (hack so GR doesn't throw a fit)
 #
 # Generalized Linear Models (GLMs) are a family of statistical models where the prediction
 # is based on a function of a linear predictor $x_^T\beta$ where $x$ is a vector of features
-# and $\beta$ is the parameter vector.
+# and $\beta$ is the parameter vector.  The two most common GLMs are linear and logistic 
+# regression.
+
+# The GLM package
 #
-# GLMs extend to other types of the data, but the most common types of GLMs are linear
-# and logistic regression.
+# 
 
 # ## Linear Regression
+
 
 # ## Logistic Regression
